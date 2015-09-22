d86ae86aea88b946305d0b038f03ef6cb4ed7dc4
Łukasz Żarnowiecki <dolohow@outlook.com>
2015-09-22 14:46:21 +0200

    middleware: Add mocks and types declarations

---
+++ middleware/types.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+// Middleware type is an adapter to allow the use of ordinary functions as
+// handlers.
+type Middleware func(http.Handler) http.Handler
+
+// HTTPHandlerMock structure is usead for injecting dependency of type
+// http.Handler.
+type HTTPHandlerMock struct {
+	w http.ResponseWriter
+	r *http.Request
+}
+
+func (h *HTTPHandlerMock) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
